7a62355b8b1a9ab82718e789adfe19dc47d4da68
Murad Ahmed <82479842+muradahmed25@users.noreply.github.com>
2021-11-25 17:00:02 +0000

    Add files via upload
    All the files for the math home page

---
+++ script.js
@@ -0,0 +1,17 @@
+// var  leftBar = document.getElementById('leftsidebar');
+// console.log(leftBar);
+
+// var para = document.getElementsByClassName("intro");
+// var content = para.innerHTML;
+// console.log(content);
+//
+// var mainTitle = document.getElementById('title');
+// console.log("This is an element of type:", mainTitle.nodeType);
+// console.log("The inner HTML is ", mainTitle.innerHTML);
+// console.log("There are this many child objects", mainTitle.childNodes);
+
+var myLinks = document.getElementsByTagName('a');
+var firstLink = myLinks[0].getAttribute("href");
+console.log(firstLink.length);
+// var title = document.getElementById('title');
+// title.getAttribute("id");
